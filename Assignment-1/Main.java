ca787a9d77b11fd003b56c047c7378b5bacfaa4a
Rajesh Mohan <rittiksingh2814@gmail.com>
2026-02-05 00:32:54 +0530

    Added Java Experiment 1 and 2

---
+++ Assignment-1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MathOperation obj = new MathOperation();
+
+        obj.calculate(5, 3);
+        obj.calculate(2.5, 4.0);
+        obj.calculate(6);
+    }
+}
---
+++ Assignment-1/MathOperation.java
@@ -0,0 +1,14 @@
+class MathOperation {
+
+    void calculate(int a, int b) {
+        System.out.println("Addition: " + (a + b));
+    }
+
+    void calculate(double a, double b) {
+        System.out.println("Multiplication: " + (a * b));
+    }
+
+    void calculate(int a) {
+        System.out.println("Square: " + (a * a));
+    }
+}
